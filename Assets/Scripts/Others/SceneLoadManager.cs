89645c301d8ba7255fbd010ca4379d5514616a5b
Koryuan <andre.saputra@shintavr.com>
2022-06-22 17:40:34 +0700

    Main Menu
    Add Main Menu Scene
    Add Input for Main Menu
    Add Tutorial Scene

---
+++ Assets/Scripts/Others/SceneLoadManager.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoadManager
+{
+    public static void LoadTutorialMap() => SceneManager.LoadScene("Tutorial Scene");
+}
---
+++ Assets/Scripts/UI/HoverButton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[Serializable] public class HoverButton : Button, IPointerEnterHandler, IPointerExitHandler
+{
+    public int buttonNumber = 0;
+    public event Action<int> OnPointerHover;
+
+    public override void OnPointerEnter(PointerEventData eventData) => OnPointerHover?.Invoke(buttonNumber);
+}
