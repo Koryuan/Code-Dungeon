45681b0f4fc1a1c2c9ecc47ec748dcf2d08d6b20
Koryuan <andre.saputra@shintavr.com>
2022-12-14 18:29:50 +0700

    Add Minor
    Add Minor

---
+++ Assets/Scripts/Code Machine/Compiler/New Compiler/CompilePrintStatic.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class CompilePrintStatic : CodeCompiler
+{
+    private string[] m_printedText;
+
+    public override string[] PrintCompile(string[] InputFieldData) => CheckText(m_printedText);
+}
